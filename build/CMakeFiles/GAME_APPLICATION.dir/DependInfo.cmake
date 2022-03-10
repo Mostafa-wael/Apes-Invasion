81666ef239ebcc47fbd2894542905657734f3ddf
Tarek <tarekspeed7@gmail.com>
2022-03-10 22:47:57 +0200

    Initial config with phase 1 files and vscode configurations to build and run with cmake

---
+++ build/CMakeFiles/GAME_APPLICATION.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/vendor/glad/src/gl.c" "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/CMakeFiles/GAME_APPLICATION.dir/vendor/glad/src/gl.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../source/common"
+  "../vendor/glfw/include"
+  "../vendor/glad/include"
+  "../vendor/glm"
+  "../vendor/imgui"
+  "../vendor/utils"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/source/common/application.cpp" "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/CMakeFiles/GAME_APPLICATION.dir/source/common/application.cpp.o"
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/source/common/shader/shader.cpp" "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/CMakeFiles/GAME_APPLICATION.dir/source/common/shader/shader.cpp.o"
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/source/common/texture/screenshot.cpp" "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/CMakeFiles/GAME_APPLICATION.dir/source/common/texture/screenshot.cpp.o"
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/source/main.cpp" "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/CMakeFiles/GAME_APPLICATION.dir/source/main.cpp.o"
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/vendor/imgui/imgui.cpp" "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui.cpp.o"
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/vendor/imgui/imgui_demo.cpp" "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_demo.cpp.o"
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/vendor/imgui/imgui_draw.cpp" "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_draw.cpp.o"
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/vendor/imgui/imgui_impl/imgui_impl_glfw.cpp" "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_impl/imgui_impl_glfw.cpp.o"
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/vendor/imgui/imgui_impl/imgui_impl_opengl3.cpp" "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_impl/imgui_impl_opengl3.cpp.o"
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/vendor/imgui/imgui_widgets.cpp" "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_widgets.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../source/common"
+  "../vendor/glfw/include"
+  "../vendor/glad/include"
+  "../vendor/glm"
+  "../vendor/imgui"
+  "../vendor/utils"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/FC8006E78006A7EA/Users/tarek/Desktop/College/Year3/Second Term/CG/project/CMP3060 Phase 1 - Student Version/build/vendor/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/GAME_APPLICATION.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "../bin/GAME_APPLICATION"
+  "../bin/GAME_APPLICATION.pdb"
+  "CMakeFiles/GAME_APPLICATION.dir/source/common/application.cpp.o"
+  "CMakeFiles/GAME_APPLICATION.dir/source/common/shader/shader.cpp.o"
+  "CMakeFiles/GAME_APPLICATION.dir/source/common/texture/screenshot.cpp.o"
+  "CMakeFiles/GAME_APPLICATION.dir/source/main.cpp.o"
+  "CMakeFiles/GAME_APPLICATION.dir/vendor/glad/src/gl.c.o"
+  "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui.cpp.o"
+  "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_impl/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_impl/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_widgets.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/GAME_APPLICATION.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
