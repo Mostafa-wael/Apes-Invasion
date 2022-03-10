584900809b17b577f4141b7667dde0507d44f21b
We2Am-BaSsem <58189568+We2Am-BaSsem@users.noreply.github.com>
2022-03-11 01:31:51 +0200

    finish shader class todos
    not finished yet sorry :"(

--- source/common/shader/shader.hpp
+++ source/common/shader/shader.hpp
@@ -28,26 +28,32 @@ namespace our {
 
         void use() { 
             //TODO: call opengl to use the program identified by this->program
+            glUseProgram(this->program);
         }
 
         GLuint getUniformLocation(const std::string &name) {
             //TODO: call opengl to get the uniform location for the uniform defined by name from this->program
+            return glGetUniformLocation(this->program, name.c_str());
         }
 
         void set(const std::string &uniform, GLfloat value) {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform1f(getUniformLocation(uniform), value);
         }
 
         void set(const std::string &uniform, glm::vec2 value) {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform2f(getUniformLocation(uniform), value.x, value.y);
         }
 
         void set(const std::string &uniform, glm::vec3 value) {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform3f(getUniformLocation(uniform), value.x, value.y, value.z);
         }
 
         void set(const std::string &uniform, glm::vec4 value) {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform4f(getUniformLocation(uniform), value.x, value.y, value.z, value.w);
         }
 
 
