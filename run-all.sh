698988b059c1c59088a48674b801f497d605edaf
Mostafa-wael <mostafa.w.k000@gmail.com>
2022-04-23 17:26:42 +0200

    Add the Mesh requirments to to the test

--- run-all.sh
+++ run-all.sh
@@ -1,16 +1,26 @@
 # Clear-Color Scenes
-./bin/GAME_APPLICATION -f=2 -c='config/blue-screen.json'
-./bin/GAME_APPLICATION -f=2 -c='config/yellow-screen.json'
+# ./bin/GAME_APPLICATION -f=2 -c='config/blue-screen.json'
+# ./bin/GAME_APPLICATION -f=2 -c='config/yellow-screen.json'
 
 # Shapes Scenes
-./bin/GAME_APPLICATION -f=2 -c='config/circle01.json'
-./bin/GAME_APPLICATION -f=2 -c='config/circle02.json'
+# ./bin/GAME_APPLICATION -f=2 -c='config/circle01.json'
+# ./bin/GAME_APPLICATION -f=2 -c='config/circle02.json'
 
-./bin/GAME_APPLICATION -f=2 -c='config/square01.json'
-./bin/GAME_APPLICATION -f=2 -c='config/square02.json'
+# ./bin/GAME_APPLICATION -f=2 -c='config/square01.json'
+# ./bin/GAME_APPLICATION -f=2 -c='config/square02.json'
 
-./bin/GAME_APPLICATION -f=2 -c='config/diamond01.json'
-./bin/GAME_APPLICATION -f=2 -c='config/diamond02.json'
+# ./bin/GAME_APPLICATION -f=2 -c='config/diamond01.json'
+# ./bin/GAME_APPLICATION -f=2 -c='config/diamond02.json'
 
-./bin/GAME_APPLICATION -f=2 -c='config/line01.json'
-./bin/GAME_APPLICATION -f=2 -c='config/line02.json'
+# ./bin/GAME_APPLICATION -f=2 -c='config/line01.json'
+# ./bin/GAME_APPLICATION -f=2 -c='config/line02.json'
+
+./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-0.jsonc'
+./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-1.jsonc'
+./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-2.jsonc'
+./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-3.jsonc'
+
+./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-0.jsonc'
+./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-1.jsonc'
+./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-2.jsonc'
+./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-3.jsonc'
