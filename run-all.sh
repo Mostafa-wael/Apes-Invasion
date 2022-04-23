46b638e972a49cf4429838752e77196f60f00f89
Mostafa-wael <mostafa.w.k000@gmail.com>
2022-04-23 17:59:43 +0200

    Transformation Req is tested

--- run-all.sh
+++ run-all.sh
@@ -15,12 +15,16 @@
 # ./bin/GAME_APPLICATION -f=2 -c='config/line01.json'
 # ./bin/GAME_APPLICATION -f=2 -c='config/line02.json'
 
-./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-0.jsonc'
-./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-1.jsonc'
-./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-2.jsonc'
-./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-3.jsonc'
+# Mesh 
+# ./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-0.jsonc'
+# ./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-1.jsonc'
+# ./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-2.jsonc'
+# ./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/default-3.jsonc'
 
-./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-0.jsonc'
-./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-1.jsonc'
-./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-2.jsonc'
-./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-3.jsonc'
+# ./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-0.jsonc'
+# ./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-1.jsonc'
+# ./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-2.jsonc'
+# ./bin/GAME_APPLICATION -f=2 -c='config/mesh-test/monkey-3.jsonc'
+
+# Transformmation
+./bin/GAME_APPLICATION -f=2 -c='config/transform-test/test-0.jsonc'
